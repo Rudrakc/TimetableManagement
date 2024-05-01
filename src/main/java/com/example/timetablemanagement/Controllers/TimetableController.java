7ed5face308d51069774883bd8e563ca3d710329
narasimha-1511 <s.narasimha.2005@gmail.com>
2024-05-01 15:55:20 +0530

    Added Time Table Controller APIs

--- src/main/java/com/example/timetablemanagement/Controllers/TimetableController.java
+++ src/main/java/com/example/timetablemanagement/Controllers/TimetableController.java
@@ -1,32 +1,52 @@
 package com.example.timetablemanagement.Controllers;
 
 import com.example.timetablemanagement.Models.Timetable;
-import com.example.timetablemanagement.Models.TimetableEntry;
 import com.example.timetablemanagement.Services.TimetableService;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/timetable")
 public class TimetableController {
 
+    @Autowired
     private TimetableService timetableService;
 
     public TimetableController(TimetableService timetableService) {
         this.timetableService = timetableService;
     }
 
-    @GetMapping("")
-    public Timetable getTimetable(Long id) {
-//        System.out.println("Creating timetable");
-        return null;
+    //Here we will fetch all the time tables
+    @GetMapping("/timetable")
+    public String getTimetable(Long id) {
+        return "We will fetch all the time tables soon!";
     }
 
 
-    @PostMapping("")
-    public ResponseEntity<TimetableEntry> createTimetableEntry(@RequestBody TimetableEntry timetableEntry) {
-        System.out.println("Creating timetable");
-        return timetableService.createTimetableEntry(timetableEntry);
+    //Here We will Fetch the timetable by id
+    @GetMapping("/timetable/{id}")
+    public String getTimetableById(@PathVariable Long id) {
+        return "We will fetch the timetable by id soon!";
+    }
+
+
+    //Here we will create a timetable
+    // also define the request body
+    // creating a good object for that
+    @PostMapping("/timetable")
+    public String createTimetable(@RequestBody Timetable timetable) {
+        return "We will create a timetable soon!";
+    }
+
+    //Updating an timetable enty
+    @PutMapping("/timetable/{id}")
+    public String updateTimetable(@PathVariable Long id, @RequestBody Timetable timetable) {
+        return "We will update the timetable soon!";
+    }
+
+    //Deleting a timetable entry
+    @DeleteMapping("/timetable/{id}")
+    public String deleteTimetable(@PathVariable Long id) {
+        return "We will delete the timetable soon!";
     }
 
 }
